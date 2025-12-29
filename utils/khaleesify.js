025652549e0f35393980e056717c829328c70098
metastasio <myaldzina.m@gmail.com>
2025-12-29 14:38:35 +0300

    Fix naming

--- utils/khaleesify.js
+++ utils/khaleesify.js
@@ -1,20 +1,20 @@
 const { vocab } = require('./vocab');
 
 const khaleesify = (text) => {
-  const complexKeys = Object.keys(vocab.complex);
-  const getComplexified = text.split(' ').map((word) => {
-    let complexified = word;
+  const vocabKeys = Object.keys(vocab);
+  const modifiedText = text.split(' ').map((word) => {
+    let modifiedWord = word;
 
-    complexKeys.forEach((key) => {
-      if (complexified.includes(key)) {
-        complexified = complexified.split(key).join(vocab.complex[key]);
+    vocabKeys.forEach((key) => {
+      if (modifiedWord.includes(key)) {
+        modifiedWord = modifiedWord.split(key).join(vocab[key]);
       }
     });
 
-    return complexified;
+    return modifiedWord;
   });
 
-  return getComplexified.join(' ').replace(/ {2,}/g, ' ');
+  return modifiedText.join(' ').replace(/ {2,}/g, ' ');
 };
 
 module.exports = { khaleesify };
